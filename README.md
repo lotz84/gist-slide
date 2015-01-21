69c1f4d335b2487b357bd482f2e68fac25661e87
Tatsuya Hirose <tatsuya.hirose.0804@gmail.com>
2015-01-21 23:44:52 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+#Gist Slide
+Gist Slide は Gist に書かれたmarkdownをそのまま[remark.js](https://github.com/gnab/remark)に渡してスライドにする。ただそれだけのChrome Extensionです。
+
+[デモ用Gist](https://gist.github.com/lotz84/970978a134057b7983ee)
+
+##Install
+
+プロジェクトをローカルに落とします
+
+```bash
+$ git clone git@github.com:lotz84/gist-slide.git
+```
+
+Chrome を起動してURLバーから<chrome://extensions/>にアクセスします
+
+パッケージ化されていない拡張機能を読み込む ボタンをクリックして先ほど落としたフォルダを選択してください
+
+これで完了です！
+
+##LICENSE
+MIT License
---
+++ script.js
@@ -0,0 +1,29 @@
+var start = function() {
+  var makeSlide = function(){
+    var url = $(".gist-content .meta .actions .button-group")
+              .first()
+              .find(".raw-url")
+              .attr("href");
+    $("body").html("");
+    $.get(url, function(md){
+      $("body").html($("<textarea />").attr("id","source").text(md));
+      remark.create();
+    });
+  }
+  var url = $(".gist-content .meta")
+            .filter(function(i, obj){return $(obj).find("strong.file-name").text() == "style.css";})
+            .find(".raw-url").attr("href")
+  $.get(url+"/raw/style.css")
+  .then(function(style){
+    $("head").append($("<style />").text(style));
+    makeSlide();
+  }).fail(makeSlide);
+}
+
+$(function(){
+  var btn = $("<a />")
+            .attr("class", "minibutton")
+            .text("▶プレゼンテーションを開始")
+            .on("click", start);
+  $(".gist-content .meta .button-group").first().prepend(btn);
+});
